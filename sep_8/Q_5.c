81eb3196d40a23e27cdfe9fd8b9dd4c52b6a3b6b
4prathamesh <wprathamesh2@gmail.com>
2023-09-08 21:52:37 +0530

    enter

--- sep_8/Q_5.c
+++ sep_8/Q_5.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int i,no;
+    int i,no,a[5],index,size=5,ele;
 
     printf("enter the array element:\n");
     for(i=0;i<5;i++)
     {
-        
+        scanf("%d",&a[i]);
     }
+
+    printf("enter the index and element \n");
+    scanf("%d%d",&index,&ele);
+
+    for(i=4;i<=index;i--)
+    {
+        a[i+1]=a[i];
+
+    }
+    size++;
+
+    a[index]=ele;
+
+    printf("after adding element : \n");
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    return 0;
 }
