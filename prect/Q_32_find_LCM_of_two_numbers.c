317d410c0ae56777a0417d73b9857dc3fd720621
4prathamesh <wprathamesh2@gmail.com>
2023-09-27 00:42:26 +0530

    Recursion

---
+++ prect/Q_32_find_LCM_of_two_numbers.c
@@ -0,0 +1,31 @@
+// 32.Write a C program to find LCM of two numbers.
+#include<stdio.h>
+// int p;
+void lcm(int no1,int no2,int i,int p)
+{
+    if(i%no1==0 && i%no2==0)
+    {
+        printf("%d",i);
+    }
+    else{
+        i=p+i;
+        lcm(no1,no2,i,p);
+    }
+}
+int main()
+{
+    int num1,num2,i;
+    printf("enter the two number ");
+    scanf("%d%d",&num1,&num2);
+    if(num1>num2)
+    {
+        i=num1;
+    }
+    else
+    {
+        i=num2;
+    }
+    int p=i;
+    lcm(num1,num2,i,p);
+    return 0;
+}
