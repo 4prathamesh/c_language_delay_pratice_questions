e67553afbaa37c7fe11f111fa76a05e1f8ffa216
4prathamesh <wprathamesh2@gmail.com>
2023-10-15 15:58:46 +0530

    Function recursion

--- prect/Q_6_second_larg_element_in_array_using_recursion.c
+++ prect/Q_6_second_larg_element_in_array_using_recursion.c
@@ -1,22 +1,22 @@
 // 6.Program to find the second largest element of an array
 #include<stdio.h>
-int i=0,max=0,smax;
-void Slarge(int a[])
+int i=0,smax;
+void Slarge(int a[],int max)
 {  
     if(i<5){
         if(a[i]>max){  // max=7 ,smax=0
             smax=max;
             max=a[i];
             i++;
-            Slarge(a);
+            Slarge(a,max);
         }else if(a[i]>smax && a[i]<max){
                smax=a[i];
                i++;
-               Slarge(a);
+               Slarge(a,max);
         }
         else{
             i++;
-            Slarge(a);
+            Slarge(a,max);
         }
 
     } else{
@@ -32,6 +32,7 @@ int main()
     {
         scanf("%d",&a[i]);
     }
-    Slarge(a);
+    int m=a[0];
+    Slarge(a,m);
     return 0;
 }
