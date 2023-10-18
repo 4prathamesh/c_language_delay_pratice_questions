e3e161aae3f77cfb6ab6ad37cf0649b24d7490c3
4prathamesh <wprathamesh2@gmail.com>
2023-10-18 10:15:25 +0530

    File handling question

--- oct_13/Q_7_count_file.c
+++ oct_13/Q_7_count_file.c
@@ -2,10 +2,11 @@
 #include<stdio.h>
 int main()
 {
-    FILE *f;
+    FILE *f,*f2;
     f=fopen("comper1.txt","r");
+    f2=fopen("p.txt","w");
     printf("file is open for read\n");
-    char ch,ch1[100];
+    char ch;
     int chara=0,lin=1,word=1;
     while((ch=fgetc(f))!=EOF)
     {
@@ -18,10 +19,22 @@ int main()
         {
             word++;
         }
+        fputc(ch,f2);
     }
     printf("character in file =%d",chara);
     printf("\nline in file =%d",lin);
     printf("\nword in file =%d",word);
+    fclose(f);
+    fclose(f2);
+    f=fopen("comper1.txt","r");
+
+    char ch1[100],chc[20];
+    int wor=0;
+    while((fscanf(f,"%s",ch1))!=EOF)  // duble space
+    {
+        wor++;
+    }
+    printf("\nword in file =%d",wor);
     fclose(f);
     return 0;
 }
